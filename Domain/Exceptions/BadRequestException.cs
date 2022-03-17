32f3dc1dee100ba1cbbbcd0c842ce885f21648c7
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 14:21:38 +0100

    Add BadRequestException

---
+++ Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
