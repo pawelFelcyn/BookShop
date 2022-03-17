e62759f486905509207c062529d03a7bdad5b0ae
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 14:01:16 +0100

    Add MappingProfile
    This class finds all types which implements IMapConfigurator present
    in executing assembly, creates their instances and invokes
    ConfigureMapping method.

---
+++ Application/Mapping/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetTypes()
+                        .Where(t => typeof(IMapCongigurator).IsAssignableFrom(t)
+                        && !t.IsInterface).ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod(nameof(IMapCongigurator.ConfigureMapping));
+
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
