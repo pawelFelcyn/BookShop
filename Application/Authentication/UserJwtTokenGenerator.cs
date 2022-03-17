da297326fbe608292ec24d4f15cc62351e9cd284
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 14:30:27 +0100

    Add UserJwtTokenGenerator
    This class is for generating token for User, which includes Id, Email
    and Role in claims.

---
+++ Application/Authentication/UserJwtTokenGenerator.cs
@@ -0,0 +1,17 @@
+﻿using AspNetAuthentication.Settings;
+using AspNetAuthentication.TokenGeneration;
+using Domain.Entities;
+using System.Security.Claims;
+
+namespace Application.Authentication
+{
+    public class UserJwtTokenGenerator : JwtTokenGeterator<User>
+    {
+        public UserJwtTokenGenerator(IAuthenticationSettings authenticationSettings) : base(authenticationSettings)
+        {
+            IncludeInClaims(ClaimTypes.NameIdentifier, u => u.Id.ToString());
+            IncludeInClaims(ClaimTypes.Email, u => u.Email);
+            IncludeInClaims(ClaimTypes.Role, u => u.RoleName);
+        }
+    }
+}
