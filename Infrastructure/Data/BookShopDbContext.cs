543925b215267c558c273af5a2dd902ded2b9686
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 14:55:57 +0100

    Add Users and Addresses dbsets to dbcontext
    Created extensions for ModelBuilder for creting User and Address, which
    are invoked in OnModelCreating in BookShopDbContext.

--- Infrastructure/Data/BookShopDbContext.cs
+++ Infrastructure/Data/BookShopDbContext.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
     public class BookShopDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
         public BookShopDbContext(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.OnUserCreating()
+                        .OnAddressCreating();
         }
     }
 }
---
+++ Infrastructure/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static ModelBuilder OnUserCreating(this ModelBuilder builder)
+        {
+            builder.Entity<User>(e =>
+            {
+                e.Property(u => u.Email)
+                .IsRequired();
+
+                e.Property(u => u.FirstName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+                e.Property(u => u.LastName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+                e.Property(u => u.RoleName)
+                .IsRequired();
+
+                e.Property(u => u.PasswordHash)
+                .IsRequired();
+
+                e.Property(u => u.Registered)
+                .IsRequired();
+
+                e.Property(u => u.AddressId)
+                .IsRequired();
+            });
+
+            return builder;
+        }
+
+        public static ModelBuilder OnAddressCreating(this ModelBuilder builder)
+        {
+            builder.Entity<Address>(e =>
+            {
+                e.Property(a => a.Country)
+                .HasMaxLength(30)
+                .IsRequired();
+
+                e.Property(a => a.City)
+                .HasMaxLength(30)
+                .IsRequired();
+
+                e.Property(a => a.Street)
+                .HasMaxLength(30)
+                .IsRequired();
+
+                e.Property(a => a.PostalCode)
+                .HasMaxLength(6)
+                .IsRequired();
+            });
+
+            return builder;
+        }
+    }
+}
