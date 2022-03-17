a4f9298309150ee6955690ff97a35de69d61cb35
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 13:20:01 +0100

    Add User and Address entities

---
+++ Domain/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Address : Entity
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class User : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public DateTime Registered { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RoleName { get; set; }
+        public string PasswordHash { get; set; }
+
+        public int AddressId { get; set; }
+        public virtual Address Address { get; set; }
+    }
+}
