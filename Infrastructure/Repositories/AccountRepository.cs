2801757210eb39eaabc8c8fef4525bd3056cf70a
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 15:04:36 +0100

    Add AccountRepository
    This class implements IAccountRepository, all the methods have already
    been implemented.

---
+++ Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Exceptions;
+using System.Linq;
+
+namespace Infrastructure.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BookShopDbContext _dbContext;
+
+        public AccountRepository(BookShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(User user)
+        {
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _dbContext.Users
+                   .FirstOrDefault(u => u.Email == email)
+                   ?? throw new InvalidEmailException();
+        }
+    }
+}
