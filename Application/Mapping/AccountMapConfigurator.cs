770614ed5aefb197e541947ac3d246edc4c9ce3d
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 14:09:06 +0100

    Add AccountMapConfigurator
    This class is for configuring map from RegisterDto to User.

---
+++ Application/Mapping/AccountMapConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using Application.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using System;
+
+namespace Application.Mapping
+{
+    public class AccountMapConfigurator : IMapCongigurator
+    {
+        public void ConfigureMapping(Profile profile)
+        {
+            profile.CreateMap<RegisterDto, User>()
+                .ForMember(u => u.Registered, c => c.MapFrom(r => DateTime.UtcNow))
+                .ForMember(u => u.Address, c => c.MapFrom(r => new Address()
+                {
+                    Country = r.Country,
+                    City = r.City,
+                    Street = r.Street,
+                    PostalCode = r.PostalCode
+                }));
+        }
+    }
+}
