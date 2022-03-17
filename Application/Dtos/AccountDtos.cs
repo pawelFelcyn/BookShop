eaa7119ea2b1a102d0449c023cd419c651d36038
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 13:36:11 +0100

    Add AccountDtos file
    This file includes RegisterDto and LoginDto records.

---
+++ Application/Dtos/AccountDtos.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public record RegisterDto(string FirstName, string LastName, DateTime? Birthdate, string Email,
+    string PhoneNumber, string RoleName, string Country, string City, string Street,
+    string PostalCode, string Password, string ConfirmPassword);
+
+public record LoginDto(string Email, string Password);
