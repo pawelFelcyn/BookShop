8c55cc88c64bc028f729eb8b154ae4dc3363aac6
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 13:22:39 +0100

    Add IAccountRepository
    This interface includes two methods: Add and GetByEmail.

---
+++ Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IAccountRepository
+    {
+        void Add(User user);
+        string GetByEmail(string email);
+    }
+}
