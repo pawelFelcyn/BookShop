bb3edaa7d25ddaf3ec52012e9853d9d7f321f1d4
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 13:40:51 +0100

    Add IAccountService
    This interface includes two methods: Register and GetToken.

---
+++ Application/Services/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos;
+
+namespace Application.Services
+{
+    public interface IAccountService
+    {
+        void Register(RegisterDto dto);
+        string GetToken(LoginDto dto);
+    }
+}
