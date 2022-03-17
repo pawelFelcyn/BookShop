0fad0582d49f73b4c6598730371ffe13a09716d9
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-17 14:37:00 +0100

    Add DependencyInjection in Application
    This is a static class for IServiceCollectionExtensions.
    So far there is one methid implemented  : AddApplication.
    It is for registration services in service collection.

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Application.Authentication;
+using Application.Services;
+using AspNetAuthentication.TokenGeneration;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddScoped<IAccountService, AccountService>();
+
+            services.AddScoped<ITokenGenerator<User>, UserJwtTokenGenerator>();
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+            return services;
+        }
+    }
+}
