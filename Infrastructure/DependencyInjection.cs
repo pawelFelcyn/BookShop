8743d070f02d0df3f163f227f7fa244f2fa8be97
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-19 21:13:31 +0100

    Create AddInfrastructure static method
    This methid registers EmailValidationHelper and AccountRepository in
    services collection.

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Helpers;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IEmailValidationHelper, EmailValidationHelper>();
+
+            services.AddScoped<IAccountRepository, AccountRepository>();
+
+            return services;
+        }
+    }
+}
