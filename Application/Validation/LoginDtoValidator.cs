6ca801bf4d45ab6db9bf9e2fc4f195718e74d2d8
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-03-24 14:10:40 +0100

    Add LoginDtoValidator

---
+++ Application/Validation/LoginDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using Application.Dtos;
+using FluentValidation;
+using System;
+
+namespace Application.Validation
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            SetRulesForEmail();
+            SetRulesForPassword();
+        }
+
+        private void SetRulesForEmail()
+        {
+            RuleFor(l => l.Email)
+                .NotEmpty()
+                .WithMessage("Email must not be empty");
+        }
+
+        private void SetRulesForPassword()
+        {
+            RuleFor(l => l.Password)
+                .NotEmpty()
+                .WithMessage("Password must not be empty");
+        }
+    }
+}
