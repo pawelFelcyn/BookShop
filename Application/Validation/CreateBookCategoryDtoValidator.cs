2f76501d9612e2c22e156df29f1bfdcfdc4dad7a
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-04-21 14:16:22 +0200

    Add CreateBookCategoryValidator

---
+++ Application/Validation/CreateBookCategoryDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Dtos;
+using FluentValidation;
+using System;
+
+namespace Application.Validation
+{
+    public class CreateBookCategoryDtoValidator : AbstractValidator<CreateBookCategoryDto>
+    {
+        public CreateBookCategoryDtoValidator()
+        {
+            SetRulesForName();
+        }
+
+        private void SetRulesForName()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithMessage("Name must not be empty");
+
+            RuleFor(c => c.Name)
+                .MaximumLength(30)
+                .WithMessage("The maximum length of name is 30 charascters");
+        }
+    }
+}
