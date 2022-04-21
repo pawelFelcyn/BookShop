e972f5a9369725a38d0c9f161d133784302dd7c3
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-04-21 14:38:48 +0200

    Add tests vor UpdateBookCategoryValidator

---
+++ Application.Tests/Validation/UpdateBookCategoryDtoValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Application.Dtos;
+using Application.Tests.Helpers;
+using Application.Validation;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Application.Tests.Validation
+{
+    public class UpdateBookCategoryDtoValidatorTests : IClassFixture<UpdateBookCategoryDtoValidator>
+    {
+        private readonly UpdateBookCategoryDtoValidator _validator;
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UpdateBookCategoryDtoValidatorTests(UpdateBookCategoryDtoValidator validator, ITestOutputHelper testOutputHelper)
+        {
+            _validator = validator;
+            _testOutputHelper = testOutputHelper;
+        }
+
+        public static IEnumerable<object[]> GetModelsWithResults()
+        {
+            yield return new object[] { new UpdateBookCategoryDto(string.Empty), false };
+            yield return new object[] { new UpdateBookCategoryDto(null), false };
+            yield return new object[]
+            { new UpdateBookCategoryDto("Toooooo loooooooooooooooong naaassssssssame"), false };
+            yield return new object[] { new UpdateBookCategoryDto("Good name"), true };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetModelsWithResults))]
+        public void Validate_ForGivenModel_ReturnsProperValidationResult(UpdateBookCategoryDto model, bool shouldBeValid)
+        {
+            var result = _validator.Validate(model);
+
+            _testOutputHelper.LogValidationErrorsIfExist(result);
+
+            result.IsValid.Should().Be(shouldBeValid);
+        }
+    }
+}
