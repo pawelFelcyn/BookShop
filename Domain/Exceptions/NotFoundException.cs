a55c07c54c7ae64c0e26b85fb3f720485634145b
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-04-21 14:46:45 +0200

    Add NotFoundException

---
+++ Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
