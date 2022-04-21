9cc959dde4dabbf37ec93fbc8f91b2dae8e86f6d
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-04-21 14:47:13 +0200

    Add BookCategoryNotFoundException

---
+++ Infrastructure/Exceptions/BookCategoryNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Exceptions;
+
+namespace Infrastructure.Exceptions
+{
+    internal class BookCategoryNotFoundException : NotFoundException
+    {
+        public BookCategoryNotFoundException() : base("Category with given id was not found in database")
+        {
+
+        }
+    }
+}
