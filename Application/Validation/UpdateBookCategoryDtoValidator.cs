51ef5064f04c9ddbaf590614a36ca3541d38b1f6
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-04-21 14:21:32 +0200

    Add UpdateBookCategoryDtoValidator

---
+++ Application/Validation/UpdateBookCategoryDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Dtos;
+using FluentValidation;
+using System;
+
+namespace Application.Validation
+{
+    public class UpdateBookCategoryDtoValidator : AbstractValidator<UpdateBookCategoryDto>
+    {
+        public UpdateBookCategoryDtoValidator()
+        {
+            SetRulesForName();
+        }
+
+        private void SetRulesForName()
+        {
+            RuleFor(u => u.Name)
+                .NotEmpty()
+                .WithMessage("Field name must not be empty");
+
+            RuleFor(u => u.Name)
+                .MaximumLength(30)
+                .WithMessage("The maximum length of the password is 30 characteres");
+        }
+    }
+}
