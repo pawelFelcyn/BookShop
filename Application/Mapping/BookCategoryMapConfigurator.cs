22f7ecbcaaea76f03fe825c4badb6bc901c31fa7
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-04-21 13:48:08 +0200

    Add map configuration for book category dtos

---
+++ Application/Mapping/BookCategoryMapConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapping
+{
+    public class BookCategoryMapConfigurator : IMapCongigurator
+    {
+        public void ConfigureMapping(Profile profile)
+        {
+            profile.CreateMap<BookCategory, BookCategoryDto>();
+            profile.CreateMap<CreateBookCategoryDto, BookCategory>();
+        }
+    }
+}
